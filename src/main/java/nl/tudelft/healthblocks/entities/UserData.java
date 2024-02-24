aea79ba60e0bed1cd15ccb2dd1f79e8c0d35a1bc
Jegor Zelenjak <jzelenjak@proton.me>
2024-02-24 18:25:32 +0100

    Update UserData.java

--- src/main/java/nl/tudelft/healthblocks/entities/UserData.java
+++ src/main/java/nl/tudelft/healthblocks/entities/UserData.java
@@ -15,23 +15,23 @@ import java.util.UUID;
 
 /**
  * A class that is used to store the user data.
- * Username is used to log in or to register a user (researcher).
- * UserId is used internally to identify the user (i.e. also in the UserManagement service);
+ * Username is used to log in researcher.
+ * UserId is used internally to identify the user (e.g. when deleting a user);
  *  it is also used as "Subject" in the JWT token used for the authentication.
  */
-@Entity // (name = "user_data")
-//@Table(name = "user_data")
+@Entity
 @Getter @NoArgsConstructor
 public class UserData implements UserDetails {
-
-    @Column(name = "username", length = 20, unique = true, nullable = false)
-    private String username;
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "user_id", unique = true, nullable = false, updatable = false)
     private UUID userId;
 
+    @Column(name = "username", length = 30, unique = true, nullable = false)
+    private String username;
+
+    @Setter
     @Column(name = "password", length = 128, nullable = false)
     private String password;
 
@@ -75,11 +75,16 @@ public class UserData implements UserDetails {
     /**
      * Creates a UserData object with the specified values.
      *
-     * @param username  the username of the user (a unique identifier used to log in)
-     * @param password  the password of the user (will be hashed)
-     * @param role      the role of the user ("admin" or "researcher")
+     * @param username      the username of the user (a unique identifier used to log in)
+     * @param password      the password of the user (will be hashed)
+     * @param email         the email of the user (must be unique)
+     * @param firstName     the first name of the user
+     * @param lastName      the last name of the user
+     * @param affiliation   the affiliation of the user (i.e. university)
+     * @param role          the role of the user ("admin" or "researcher")
      */
-    public UserData(String username, String password, String email, String firstName, String lastName, String affiliation, UserRole role) {
+    public UserData(String username, String password, String email, String firstName,
+                    String lastName, String affiliation, UserRole role) {
         this.username = username;
         this.password = password;
         this.email = email;
@@ -104,16 +109,16 @@ public class UserData implements UserDetails {
     }
 
     /**
-     * Checks whether another user is equal to this user.
+     * Checks whether another user is equal to this user, based on their userIDs.
      *
      * @param other another user to compare to
-     * @return true if another user is equal to this user, i.e. if their usernames are equal, false otherwise
+     * @return true if another user is equal to this user, i.e. if their userIDs are equal, false otherwise
      */
     @Override
     public boolean equals(Object other) {
         if (this == other) return true;
         if (!(other instanceof UserData otherUserData)) return false;
-        return Objects.equals(this.username, otherUserData.username);
+        return Objects.equals(this.userId, otherUserData.userId);
     }
 
     /**
